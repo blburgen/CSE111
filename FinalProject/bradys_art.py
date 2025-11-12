d750871e65151d82e893e4a8f1998aaff01a289a
Brady Burgener <bradylbsee@gmail.com>
2025-11-12 12:57:12 -0700

    adds functions to final project

--- FinalProject/bradys_art.py
+++ FinalProject/bradys_art.py
@@ -2,27 +2,61 @@
     Author: Brady Burgener
     
     Time Track:
-        11/11/2025: 1hr
-        11/12/2025: 6-7am
+        11/11/2025: 1.5hr
+        11/12/2025: 1.5hr + 1hr
+        11/12/2025: 
 """
 import datetime
 import csv
 
 
 def main():
-    pass
+    art_dict = {}
+    art_dict = read_dictionary("art.csv", 0)
+    print(art_dict)
+    next_key = find_next_key(art_dict)
+    print(next_key)
+    new_entry=['not me',"Brady Burgener",0, "Oil"]
+    write_to_file("art.csv", next_key, new_entry)
 
 """
-    read_dictionary(file_name, index) return dictionary
+    read_dictionary(filename, key_column_index) return dictionary
 """
-def read_dictionay(file_name, index):
-    pass
+def read_dictionary(filename, key_column_index=0):
+    file_dict = {}
+    with open(filename, 'rt') as file:
+        
+        reader = csv.reader(file)
+        
+        next(reader)
+        for row_list in reader:
+            if len(row_list) != 0:
+                key = row_list[key_column_index]
+                file_dict[key] = row_list
+                
+    return file_dict
 
 """ 
-    write_to_file(file_name, file_dict) return null
+    finds next dictionary key if it is an integer
+    returns a int
 """
-def write_to_file(file_name, file_dict):
-    pass
+def find_next_key(dict):
+    if len(dict) != 0:
+        next_key = int(list(dict.keys())[-1]) + 1
+    else:
+        next_key = 1
+    return next_key
+
+""" 
+    write_to_file(file_name, new_row) return null
+    writes a new entry to the csv file
+"""
+def write_to_file(filename, next_key, new_row):
+    with open(filename, 'at', newline='') as file:
+        writer = csv.writer(file)
+        new_row.insert(0,next_key)
+        writer.writerow(new_row)
+        
 
 """ 
     calculate_art_cost(width, length, skill=0) return float
