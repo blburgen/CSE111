e5b32d21cc9612a996d12618685578f77d95dece
Brady Burgener <bradylbsee@gmail.com>
2025-11-12 08:05:14 -0700

    adds brady_art

---
+++ FinalProject/bradys_art.py
@@ -0,0 +1,40 @@
+""" 
+    Author: Brady Burgener
+    
+    Time Track:
+        11/11/2025: 1hr
+        11/12/2025: 6-7am
+"""
+import datetime
+import csv
+
+
+def main():
+    pass
+
+"""
+    read_dictionary(file_name, index) return dictionary
+"""
+def read_dictionay(file_name, index):
+    pass
+
+""" 
+    write_to_file(file_name, file_dict) return null
+"""
+def write_to_file(file_name, file_dict):
+    pass
+
+""" 
+    calculate_art_cost(width, length, skill=0) return float
+"""
+def calculate_art_cost(width, length, skill=0):
+    pass
+
+""" 
+    search_dictionary(file_dict, input_word) return list
+"""
+def search_dictionary(file_dict, input_word):
+    pass
+
+if __name__ == "__main__":
+    main()
